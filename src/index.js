269bb36e70ac0949b51a50c51b55345820b41e63
sachins <39sachen@gmail.com>
2018-07-25 15:09:06 +0300

    final updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+
+
+
+
+import App from './App';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('#container'));
